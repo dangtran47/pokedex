b5f2e4452fcfa5f83e2881dabbaefa0ad1714694
Dang Tran <haidangc23hn2@gmail.com>
2023-09-26 22:07:02 +0700

    Config React Query + HeadlessUI + Tailwind

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,6 +1,5 @@
 import { describe, it, expect, afterEach } from 'vitest';
 import { render, screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import App from '../App';
 
@@ -9,31 +8,8 @@ describe('Renders main page correctly', () => {
     cleanup();
   });
 
-  it('Should render the page correctly', () => {
+  it('renders switch', () => {
     render(<App />);
-    const h1 = screen.queryByText('Vite + React');
-
-    expect(h1).not.toBeNull();
-  });
-
-  it('Should show the button count set to 0', () => {
-    render(<App />);
-    const button = screen.queryByText('count is 0');
-
-    expect(button).not.toBeNull();
-  });
-
-  it('Should show the button count set to 3', async () => {
-    const user = userEvent.setup();
-    render(<App />);
-    const button = screen.queryByText('count is 0');
-
-    expect(button).not.toBeNull();
-
-    await user.click(button as HTMLElement);
-    await user.click(button as HTMLElement);
-    await user.click(button as HTMLElement);
-
-    expect(button?.innerHTML).toBe('count is 3');
+    expect(screen.getByTestId("switch")).toBeDefined();
   });
 })
