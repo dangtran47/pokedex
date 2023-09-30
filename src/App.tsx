0eafba06ae08c570ae14c92564a95fdeef9a0d57
Dang Tran <haidangc23hn2@gmail.com>
2023-09-30 09:22:40 +0700

    Setup eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import { Switch } from '@headlessui/react'
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+import { useQuery } from "react-query";
+
+import "./App.css";
 
 function App() {
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(false);
+
+  const { isLoading, error, data } = useQuery(
+    "repoData",
+    () =>
+      fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+        (res) => res.json(),
+      ),
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred";
 
   return (
-    <Switch
-      checked={checked}
-      onChange={setChecked}
-      className={`${
-        checked ? 'bg-blue-600' : 'bg-gray-200'
-      } relative inline-flex h-6 w-11 items-center rounded-full`}
-      data-testid="switch"
-    >
-      <span
+
+
+    <div>
+      <Switch
+        checked={checked}
+        onChange={setChecked}
         className={`${
-          checked ? 'translate-x-6' : 'translate-x-1'
-        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-      />
-    </Switch>
-  )
+          checked ? "bg-blue-600" : "bg-gray-200"
+        } relative inline-flex h-6 w-11 items-center rounded-full`}
+        data-testiq="switch"
+      >
+        <span
+          className={`${
+            checked ? "translate-x-6" : "translate-x-1"
+          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+        />
+      </Switch>
+
+      <div>
+        <h1>{data.name}</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
 )
