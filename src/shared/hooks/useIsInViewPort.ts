27be3d6359132636211b6c5f532cd2846fcf10ef
Dang Tran <haidangc23hn2@gmail.com>
2023-10-02 00:34:52 +0700

    Update lint-stage

--- src/shared/hooks/useIsInViewPort.ts
+++ src/shared/hooks/useIsInViewPort.ts
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 
-const useIsInViewport = (ref) => {
+const useIsInViewport = (ref: { current: HTMLElement | null }) => {
   const [isIntersecting, setIsIntersecting] = useState(false);
 
   const observer = useMemo(() => new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting)), []);
